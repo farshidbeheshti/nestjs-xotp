ea21b5ff8f0b9efe2f5ccb0cd2a043f5cb521494
farshidbeheshti <farshid.beheshti@gmail.com>
2025-04-02 20:27:34 +0330

    feat: just a wrapper around the xotp secret.

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export * from './xotp.hotp.service';
 export * from './xotp.totp.service';
 export * from './xotp.service';
+export * from './xotp.secret.service';
---
+++ src/services/xotp.secret.service.ts
@@ -0,0 +1,3 @@
+import { Secret } from 'xotp';
+
+export class XOTPSecret extends Secret {}
